31db0cc48038aa1e291508b4897a4a33495febf2
sannepetersvirtualgraphicdesigner <60850206+sannepetersvirtualgraphicdesigner@users.noreply.github.com>
2021-12-11 09:29:00 +0100

    added javascript date script

--- src/app.js
+++ src/app.js
@@ -1,40 +1,57 @@
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  moscow: {
-    temp: -5,
-    humidity: 20,
-  },
-};
+let currentTime = new Date();
+let currentHours = currentTime.getHours();
+let currentMinutes = currentTime.getMinutes();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let currentDay = days[currentTime.getDay()];
 
-let city = console.log(`Enter a city`);
-city = city.toLowerCase();
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = `${currentDay} ${currentHours}:${currentMinutes}`;
 
-if (weather[city] !== undefined) {
-  let temperature = weather[city].temp;
-  let celsiusTemp = Math.round(temperature);
-  let fahrenheitTemp = Math.round((temperature * 9) / 5 + 32);
-  let humidity = weather[city].humidity;
+// let weather = {
+//   paris: {
+//     temp: 19.7,
+//     humidity: 80,
+//   },
+//   tokyo: {
+//     temp: 17.3,
+//     humidity: 50,
+//   },
+//   lisbon: {
+//     temp: 30.2,
+//     humidity: 20,
+//   },
+//   "san francisco": {
+//     temp: 20.9,
+//     humidity: 100,
+//   },
+//   moscow: {
+//     temp: -5,
+//     humidity: 20,
+//   },
+// };
 
-  alert(
-    `It is currently ${celsiusTemp}°C (${fahrenheitTemp}°F) in ${city} with a humidity of ${humidity}%`
-  );
-} else {
-  alert(
-    `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
-  );
-}
+// let city = console.log(`Enter a city`);
+// city = city.toLowerCase();
+
+// if (weather[city] !== undefined) {
+//   let temperature = weather[city].temp;
+//   let celsiusTemp = Math.round(temperature);
+//   let fahrenheitTemp = Math.round((temperature * 9) / 5 + 32);
+//   let humidity = weather[city].humidity;
+
+//   alert(
+//     `It is currently ${celsiusTemp}°C (${fahrenheitTemp}°F) in ${city} with a humidity of ${humidity}%`
+//   );
+// } else {
+//   alert(
+//     `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
+//   );
+// }
